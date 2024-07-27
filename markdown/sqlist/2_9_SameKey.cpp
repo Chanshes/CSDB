4779dbbfd167a0e9c609411ca4298c667d179aa7
ChanShes <laixingchen530@163.com>
2024-07-28 00:02:58 +0800

    c->cpp

--- markdown/sqlist/2_9_SameKey.cpp
+++ markdown/sqlist/2_9_SameKey.cpp
@@ -1,13 +1,17 @@
 //使用三个下标变量从小到大遍历数组。当三个下标变量指向的元素相等时，输出并向前推进指针，
 //否则仅移动小于最大元素的下标变量，直到某个下标变量移除数组范围，即可停止。
+#include <stdio.h>
+#include <math.h>
+
 void samekey(int A[], int B[], int C[])
 {
-    int i=0;j=0;k=0;                    //定义三个工作指针
+    int i=0,j=0,k=0;                    //定义三个工作指针
+    int n = min(min(sizeof(A)/4,sizeof(B)/4),sizeof(C)/4);
     while (i < n && j < n && k < n)     //相同则输出，并集体后移
     {
         if (A[i] == B[j] && B[j] == C[k])
         {
-            printf("%d\n",A[i]);
+            printf("%d\n", A[i]);
             i++;j++;k++;
         }
         else
@@ -18,4 +22,14 @@ void samekey(int A[], int B[], int C[])
             if (C[k] < maxNum) k++;
         }
     }
+}
+
+int min(int A, int B)
+{
+    return A < B ? A : B;
+}
+
+int max(int A, int B)
+{
+    return A > B ? A : B;
 }
