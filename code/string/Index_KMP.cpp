5d0f4335347e41cba5714c21e5dc06ae2cde25d7
chanshes <chanshes@outlook.com>
2024-07-29 18:58:25 +0800

    add: get_nextval

--- code/string/Index_KMP.cpp
+++ code/string/Index_KMP.cpp
@@ -16,6 +16,25 @@ void get_next(SString T, int next[])
     }
 }
 
+void get_nextval(SString T, int nextval[])
+{
+    int i=1,j=0;
+    nextval[1]=0;
+    while(i<T.length)
+    {
+        if(j==0||T.ch[i]==T.ch[j])
+        {
+            ++i;++j;
+            if(T.ch[i]!=T.ch[j])
+                nextval[i]=j;
+            else
+                nextval[i]=nextval[j];
+        }
+        else
+            j=nextval[j];
+    }
+}
+
 int Index_KMP(SString S, SString T, int next[])
 {
     int i=1, j=1;
