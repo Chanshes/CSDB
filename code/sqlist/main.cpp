142eaba1a3369159cfbf7b444c18a39ad0944c77
chanshes <chanshes@outlook.com>
2024-08-11 21:14:57 +0800

    sort

--- code/sqlist/main.cpp
+++ code/sqlist/main.cpp
@@ -1,22 +1,22 @@
 #include "sqlist.cpp"
+#include <math.h>
+#include <ctime>
 
 bool Reverse(SqList &L);
+bool BubbleSort(SqList &L);
 
 int main(void)
 {
     SqList L;
     InitList(L);
-    L.length = 7;
-    L.data[0] = 81;
-    L.data[1] = 15;
-    L.data[2] = 27;
-    L.data[3] = 34;
-    L.data[4] = 58;
-    L.data[5] = 13;
-    L.data[6] = 75;
-    OutputDataStream(L);
+    L.length = 8;
+    srand(time(nullptr));
+    for(int i=0;i<8;i++)
+        L.data[i]=rand()%99;
 
-    Reverse(L);
+    OutputDataStream(L);
+    BubbleSort(L);
+    //Reverse(L);
     OutputDataStream(L);
 
     return 0;
@@ -31,6 +31,26 @@ bool Reverse(SqList &L){
         temp = L.data[i];
         L.data[i] = L.data[L.length-i-1];
         L.data[L.length-i-1] = temp;
+    }
+    return true;
+}
+
+bool BubbleSort(SqList &L){
+    ElemType temp;
+    for(int i=0;i<L.length-1;i++){
+        bool flag=false;
+        for(int j=L.length-1;j>i;j--){
+            if(L.data[j-1]>L.data[j]){
+                temp=L.data[j-1];
+                L.data[j-1]=L.data[j];
+                L.data[j]=temp;
+                flag=true;
+            }
+        }
+        OutputDataStream(L);
+        if(flag==false){
+            return true;
+        }
     }
     return true;
 }
