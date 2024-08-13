538c687a683c3272812735f70cf6d4b77efcd31c
chanshes <chanshes@outlook.com>
2024-08-13 18:25:14 +0800

    2_10 Completed

--- markdown/2_2/convers.cpp
+++ markdown/2_2/convers.cpp
@@ -3,11 +3,13 @@
 #define MaxSize 50
 
 typedef int ElemType;
+//最大长度为50的顺序表
 typedef struct Llist{
     ElemType data[MaxSize];
     int length;
 }Llist;
 
+//打印数组
 int Araay_print(int A[], int n){
     int i;
     for(i=0;i<n;i++)
@@ -15,7 +17,14 @@ int Araay_print(int A[], int n){
     printf("\n");
     return i;
 }
-
+int Araay_print(Llist L){
+    for(int i=0;i<L.length;i++){
+        printf("%d\t",L.data[i]);
+    }
+    printf("\n");
+    return L.length;
+}
+//Llist顺序表转数组
 bool excLlisttoArray(Llist L,int A[],int &n){
     n=L.length;
     for(int i=0;i<=L.length;i++){
@@ -23,11 +32,16 @@ bool excLlisttoArray(Llist L,int A[],int &n){
     }
     return true;
 }
-
-bool excArraytoLlist(Llist L,int A[],int n){
+//数组转顺序表Llist
+bool excArraytoLlist(Llist &L,int A[],int n){
+    L.length=n;
+    for(int i=0;i<n;i++){
+        L.data[i]=A[i];
+    }
     return true;
 }
-
+///@brief 将数组循环左移p位
+///@param p 循环左移次数
 //时间复杂度O(n^2)
 bool convers(int A[], int n, int p){
     if(n==0)
@@ -35,14 +49,15 @@ bool convers(int A[], int n, int p){
     int temp;
     for(int i=0;i<p;i++){
         temp=A[(p-i)%n];
+        printf("i=%d,temp=%d\n",i,temp);
         for(int j=0;j<n;j++){
             A[(p-i+j)%n]=A[(p-i+j+1)%n];    //循环顺序表前移一位
         }
         A[(p-i-1)%n]=temp;                  //每次循环将temp插入到左移一位后的空位
+        Araay_print(A,n);
     }
     return true;
 }
-
 //最优解
 bool Reverse(int R[],int n,int from,int to){
     if(n==0)
@@ -81,11 +96,10 @@ bool converse(int R[],int n,int p){
 
 int main(){
     int A[] = {1,2,3,4,5,6,7,8,9,10};
-    int x,n;
-    n=sizeof(A)/sizeof(int);
-    printf("%d\n",n);
-    scanf("%d",&x);
-    converse(A,n,x%n);
-    Araay_print(A,n);
+    int x,len;
+    len=sizeof(A)/sizeof(int);    //获取数组长度
+    
+    convers(A,len,3);
+
     return 0;
 }
