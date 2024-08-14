df4e9ced49a52929cccbeb061db1f221b4c5bcce
chanshes <chanshes@outlook.com>
2024-08-14 21:22:59 +0800

    Queue commit

---
+++ markdown/3_2/Queue.cpp
@@ -0,0 +1,41 @@
+int QueueEmpty(Stack S1,Stack S2){
+    if(StackEmpty(S1)&&StackEmpty(S2))
+        return 1;
+    else
+        return 0;
+}
+
+int EnQueue(Stack &S1,Stack &S2,ElemType e){
+    if(!StackOverflow(S1)){
+        Push(S1,e);
+        return 1;
+    }
+    if(StackOverflow(S1)&&!StackEmpty(S2)){
+        printf("队列满");
+        return 0;
+    }
+    if(StackOverflow(S1)&&StackEmpty(S2)){
+        while(!StackEmpty(S1)){
+            Pop(S1,x);
+            Push(S2,x);
+        }
+    }
+    Push(S1,e);
+    return 1;
+}
+
+void DeQueue(Stack &S1,Stack &S2,ElemType &x){
+    if(!StackEmpty(S2)){
+        Pop(S2,x);
+    }
+    else if(StackEmpty(S1)){
+        printf("队列为空");
+    }
+    else{
+        while(!StackEmpty(S1)){
+            Pop(S1,x);
+            Push(S2,x);
+        }
+        Pop(S2,x);
+    }
+}
