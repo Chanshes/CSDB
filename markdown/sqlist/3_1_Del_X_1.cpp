482cb8aedaa57d5a090de5879996a5d26c4eb2b1
chanshes <chanshes@outlook.com>
2024-08-14 20:20:50 +0800

    FMT3_14 3_1_Del_X commit

---
+++ markdown/sqlist/3_1_Del_X_1.cpp
@@ -0,0 +1,52 @@
+#include "linklist.cpp"
+
+
+void Del_X_1(LinkList &L,ElemType x){
+    LNode *p=L->next,*pre=L,*q;
+    while(p!=NULL){
+        if(p->data==x){
+            q=p;
+            p=p->next;
+            pre->next=p;
+            free(q);
+        }
+        else{
+            pre=p;
+            p=p->next;
+        }
+    }
+}
+
+void Del_X_2(LinkList &L,ElemType x){
+    LNode *p=L->next,*r=L,*q;
+    while(p->data!=NULL){
+        if(p->data!=x){
+            r->next=p;
+            r=p;
+            p=p->next;
+        }
+        else{
+            q=p;
+            p=p->next;
+            free(q);
+        }
+    }
+    r->next=NULL;
+}
+
+int main(){
+    LinkList L;
+    int x;
+    InitList(L);
+    List_HeadInsert(L);
+    printLink(L);
+    printf("\ndel:");
+    scanf("%d",&x);
+    Del_X_1(L,x);
+    printLink(L);
+    printf("\ndel:");
+    scanf("%d",&x);
+    Del_X_2(L,x);
+    printLink(L);
+
+}
---
+++ markdown/sqlist/linklist.cpp
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int ElemType;
+typedef struct LNode{
+    ElemType data;
+    struct LNode *next;
+}LNode,*LinkList;
+
+//初始化链表
+bool InitList(LinkList &L){
+    if(L!=NULL)
+        return false;
+    L=(LNode*)malloc(sizeof(LNode));
+    L->next=NULL;
+    return true;
+}
+bool InitList_NoHead(LinkList &L){
+    if(L!=NULL)
+        return false;
+    L=NULL;
+    return true;
+}
+
+//获取链表长度
+int Length(LinkList L){   
+    int len=0;
+    LNode *p=L;
+    while(p->next!=NULL){
+        p=p->next;
+        len++;
+    }
+    return len;
+}
+
+//获取不带头结点的链表长度
+int Length_NoHead(LinkList &L){
+    int len=0;
+    LNode *p=L;
+    while(p!=NULL){
+        p=p->next;
+        len++;
+    }
+    return len;
+}
+
+//获取第i个元素
+LNode *GetElem(LinkList L,int i){
+    LNode *p=L;
+    int j=0;
+    while(p!=NULL&&j<i){
+        p=p->next;
+        j++;
+    }
+    return p;
+}
+
+//获取值为e的结点
+LNode *LocateElem(LinkList &L,ElemType e){
+    LNode *p=L->next;
+    while(p!=NULL&&p->data!=e){
+        p=p->next;
+    }
+    return p;
+}
+//在第i个元素后插入元素e
+bool ListInsert(LinkList &L,int i,ElemType e){
+    LNode *p=L;
+    int j=0;
+    while(p!=NULL&&j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(p==NULL)
+        return false;
+    LNode *s=(LNode*)malloc(sizeof(LNode));
+    s->data=e;
+    s->next=p->next;
+    p->next=s;
+    return true;
+}
+
+//删除第i个的元素
+bool ListDelete(LinkList &L,int i,ElemType &e){
+    LNode *p=L;
+    int j=0;
+    while(p!=NULL&&j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(p==NULL||p->next==NULL)
+        return false;
+    LNode *q=p->next;
+    e=q->data;
+    p->next=q->next;
+    free(q);
+    return true;
+}
+bool ListDeletep(LinkList &L,LNode* &q){
+    LNode *p=L;
+    while(p!=NULL&&p->next!=q){
+        p=p->next;
+    }
+    if(p==NULL||p->next==NULL)
+        return false;
+    LNode *s=q->next;
+    p->next=s;
+    free(q);
+    return true;
+}
+
+//头插法建立链表
+LinkList List_HeadInsert(LinkList &L){
+    LNode *s;int x;
+    L=(LNode*)malloc(sizeof(LNode));
+    L->next=NULL;
+    scanf("%d",&x);
+    while(x!=9999){
+        s=(LNode*)malloc(sizeof(LNode));
+        s->data=x;
+        s->next=L->next;
+        L->next=s;
+        scanf("%d",&x);
+    }
+    return L;
+}
+LinkList List_HeadInsert_NoHead(LinkList &L){
+    LNode *s;
+    int x;
+    scanf("%d",&x);
+    while(x!=9999){
+        s=(LNode*)malloc(sizeof(LNode));
+        s->data=x;
+        s->next=L;
+        L=s;
+        scanf("%d",&x);
+    }
+    return L;
+}
+//尾插法建立链表
+LinkList List_TailInsert(LinkList &L){
+    int x;
+    L=(LNode*)malloc(sizeof(LNode));
+    LNode *s,*r=L;
+    scanf("%d",&x);
+    while(x!=9999){
+        s=(LNode*)malloc(sizeof(LNode));
+        s->data=x;
+        r->next=s;
+        r=s;
+        scanf("%d",&x);
+    }
+    r->next=NULL;
+    return L;
+}
+
+int printLink(LinkList L){
+    int n=0;
+    while(L->next!=NULL){
+        printf("%d\t",L->next->data);
+        L=L->next;
+        n++;
+    }
+    return n;
+}
