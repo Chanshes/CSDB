5202f25860e2e75deb70ac6afa2a669f37cfdd19
chanshes <chanshes@outlook.com>
2024-08-14 19:27:04 +0800

    FMM3_13 committed

--- markdown/2_2/FMM3_13.cpp
+++ markdown/2_2/FMM3_13.cpp
@@ -20,11 +20,27 @@ int findMissMin(int A[],int n){
         if(B[i]==0) break;
     return i+1;
 }
+///@brief 以下是寻常逻辑查找最小未出现的最大正整数,但平均时间复杂度为O｛nlogn｝
+//先将数组排序，然后直接查找到未出现的最小正整数
+int FindMissMin(int A[],int n){
+    QuickSort(A,0,n);   //快速排序
+    int i=0,j=1;
+    while(i<n){
+        if(A[i]<0)
+            i++;        //跳过负数
+        else if(A[i]==j){
+            i++;j++;    //j从1开始往后找
+        }
+        else
+            break;
+    }
+    return j;
+}
 
 int main(){
-    int A[] = {1,2,-3,4,5,9,7,8};
+    int A[] = {1,3,2,6,4,8,-2,-7};
     printf("数组");
     Araay_print(A,8);
-    printf("中未出现的最小正整数：%d\n",findMissMin(A,8));
+    printf("中未出现的最小正整数：%d\n",FindMissMin(A,8));
     return 0;
 }
--- markdown/2_2/pub.cpp
+++ markdown/2_2/pub.cpp
@@ -41,3 +41,23 @@ bool excArraytoLlist(Llist &L,int A[],int n){
     }
     return true;
 }
+
+//快排
+int Partition(int A[],int low,int high){
+    int pivot=A[low];
+    while(low<high){
+        while(low<high&&A[high]>=pivot) --high;
+        A[low]=A[high];
+        while(low<high&&A[low]<=pivot) ++low;
+        A[high]=A[low];
+    }
+    A[low]=pivot;
+    return low;
+}
+void QuickSort(int A[],int low,int high){
+    if(low<high){
+        int pivotpos=Partition(A,low,high);
+        QuickSort(A,low,pivotpos-1);
+        QuickSort(A,pivotpos+1,high);
+    }
+}
