45216511e93e4eeb2277b339fbd791e8a3930ea1
chanshes <chanshes@outlook.com>
2024-09-28 23:36:14 +0800

    QuickSort committed

--- code/Sort/QuickSort.cpp
+++ code/Sort/QuickSort.cpp
@@ -24,26 +24,6 @@ void QuickSort(int A[],long low,long high){
     }
 }
 
-long partition(int A[],long low,long high){
-    int pivot=A[low];
-    while(low<high){
-        while(low<high&&A[high]>=pivot) --high;
-        A[low++]=A[high];
-        while(low<high&&A[low]<=pivot) ++low;
-        A[high--]=A[low];
-    }
-    A[low]=pivot;
-    return low;
-}
-
-void quicksort(int A[],long low,long high){
-    if(low<high){
-        long pivotpos=partition(A,low,high);
-        quicksort(A,low,pivotpos-1);
-        quicksort(A,pivotpos+1,high);
-    }
-}
-
 void array_print(int A[],long n){
     for(long i=0;i<n;i++){
         printf("%d  ",A[i]);
@@ -61,10 +41,5 @@ int main(){
     QuickSort(A,0,n);
     clock_t time2=clock();
     printf("\n%ld\n",time2-time1);
-    clock_t time3=clock();
-    quicksort(B,0,n);
-    clock_t time4=clock();
-    printf("\n%ld\n",time4-time3);
-    array_print(A,n);
     return 0;
 }
