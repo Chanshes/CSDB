95dcd0030fcd15385b839775c11af5f04e6810f6
chanshes <chanshes@outlook.com>
2025-01-12 16:04:28 +0800

    ALGraph.h

--- markdown/6Graph/ALGraph.h
+++ markdown/6Graph/ALGraph.h
@@ -1,10 +1,14 @@
 #define MaxVertexNum 100
 typedef int VertexType; 
+
+/** @brief 边结点
+ *  @param adjvex 该弧指向的顶点的位置 */
 typedef struct ArcNode {
     int adjvex;
     struct ArcNode *nextarc;
 }ArcNode;
 
+/// @brief 顶点结点
 typedef struct VNode{
     VertexType data;
     ArcNode *firstarc;
